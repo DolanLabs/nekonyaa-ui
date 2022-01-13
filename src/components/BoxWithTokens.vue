ea483789bd3842f51e7ecacc1194ed695339a404
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-01-13 22:11:05 +0700

    implement design tokens

---
+++ src/components/BoxWithTokens.vue
@@ -0,0 +1,27 @@
+<script>
+import designtokens from '../../DolanLabs-Brand-Guideline/designtokens.json';
+
+export default {
+  name: 'BoxWithTokens',
+  data() {
+    return {
+      colors: designtokens.global.DolanLabs.Colors.PrimaryColors.IceBlue.value,
+    };
+  },
+
+  mounted() {
+    console.log(this.colors);
+  },
+};
+</script>
+
+<template>
+  <div class="box" :style="{background: colors}" />
+</template>
+
+<style>
+.box {
+  height: 100px;
+  width: 200px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <box-with-tokens />
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import BoxWithTokens from '../components/BoxWithTokens.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    // HelloWorld,
+    BoxWithTokens,
   },
 };
 </script>
